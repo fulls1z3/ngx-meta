d9427388cf9ce0446433a7908929aafaa8e3e641
Burak Tasci <me@fulls1z3.com>
2017-02-20 15:09:38 +0300

    refactor: rename `@nglibs/metadata` package to `@nglibs/meta`
    * build: update build config
    
    * build(npm): update deps
    
    * build(webpack): update config
    
    * ci(travis): update Travis-CI config
    
    * refactor: update tsconfig
    
    * refactor: update .gitignore
    
    * refactor: update .npmignore
    
    * style(core): lint
    
    * docs: update README

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,6 +1,10 @@
 # Change Log
 All notable changes to this project will be documented in this file.
 
+## v0.2.0-rc.2 - 2017-02-23
+### Breaking change
+- `@nglibs/metadata` has been renamed to `@nglibs/meta`
+
 ## v0.2.0-rc.1 - 2017-02-17
 ### Changed
 - Code refactoring
--- index.ts
+++ index.ts
@@ -2,38 +2,39 @@
 import { NgModule, ModuleWithProviders, Optional, SkipSelf } from '@angular/core';
 
 // module
-import { MetadataLoader, MetadataStaticLoader } from './src/metadata.loader';
-import { MetadataService } from './src/metadata.service';
+import { MetaLoader, MetaStaticLoader } from './src/meta.loader';
+import { MetaService } from './src/meta.service';
 
 export * from './src/models/page-title-positioning';
-export * from './src/models/metadata-settings';
-export * from './src/metadata.loader';
-export * from './src/metadata.service';
+export * from './src/models/meta-settings';
+export * from './src/meta.loader';
+export * from './src/meta.service';
 
-export function metadataFactory(): MetadataLoader {
-    return new MetadataStaticLoader();
+// for AoT compilation
+export function metaFactory(): MetaLoader {
+    return new MetaStaticLoader();
 }
 
 /**
  * Do not specify providers for modules that might be imported by a lazy loaded module.
  */
 @NgModule()
-export class MetadataModule {
+export class MetaModule {
     static forRoot(configuredProvider: any = {
-                       provide: MetadataLoader,
-                       useFactory: (metadataFactory)
+                       provide: MetaLoader,
+                       useFactory: (metaFactory)
                    }): ModuleWithProviders {
         return {
-            ngModule: MetadataModule,
+            ngModule: MetaModule,
             providers: [
                 configuredProvider,
-                MetadataService
+                MetaService
             ]
         };
     }
 
-    constructor(@Optional() @SkipSelf() parentModule: MetadataModule) {
+    constructor(@Optional() @SkipSelf() parentModule: MetaModule) {
         if (parentModule)
-            throw new Error('MetadataModule already loaded; import in root module only.');
+            throw new Error('MetaModule already loaded; import in root module only.');
     }
 }
